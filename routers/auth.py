26b04d5722993c7c139cbc7a196158fbdcc8579b
Fabian <fabianmoraesb@gmail.com>
2026-03-09 13:35:10 +0100

    created login endpoint

--- routers/auth.py
+++ routers/auth.py
@@ -11,15 +11,13 @@ router = APIRouter(prefix="auth", tags=['auth'])
 def check_if_email_exists(email: str, session: Session) -> bool:
 
     statement = select(User).where(User.email == email)
-    
     existing_user = session.exec(statement).first()
-
-    return existing_user is not None 
-
+    return existing_user == None
+    
 @router.post("register")
 async def create_account(data: Register, session = Depends(get_session)):
     
-    if check_if_email_exists(data.email, session=session) == False:
+    if check_if_email_exists(data.email, session=session) == True:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Użytkownik o takim adresie e-mail już istnieje."
@@ -35,5 +33,25 @@ async def create_account(data: Register, session = Depends(get_session)):
 
 
 @router.post("login")
-async def login_account():
-    ...
+async def login_account(data: Login, session = Depends(get_session)):
+    
+    statement = select(User).where(User.email == data.email)
+    get_user = session.exec(statement).first()
+    
+    login_error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Nieprawidłowy e-mail lub hasło",
+        headers={"WWW-Authenticate": "Bearer"}, # Standard dla 401
+    )
+    
+    if not get_user:
+        raise login_error
+
+
+    db_password = get_user.hash_password
+
+    check_password = verify_password(hashed_password=db_password, password=data.password)
+    if check_password == False:
+        raise login_error
+    
+    return {"message": "User {data.mail} logged in", "user_id": data.user_id}
