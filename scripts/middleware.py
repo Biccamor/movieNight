308721361c71208c091e9692b9199ff0993d413d
Fabian <fabianmoraesb@gmail.com>
2026-03-19 00:26:28 +0100

    cors from fastapi page

---
+++ scripts/middleware.py
@@ -0,0 +1,27 @@
+from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware
+import time
+from routers.main import app
+
+origins = [
+    "https://groupmovie.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
--- uttils/uttils.py
+++ uttils/uttils.py
@@ -1,11 +1,14 @@
-from sqlmodel import select
+from sqlmodel import select, Session
 from database.database_setup import User, Room_Session
+from database.main_db import engine
 
 # TODO: sprawdz czy napewno dobry jest ten user
 
-def check_if_email_exists(email: str, session: Room_Session) -> bool:
+def check_if_email_exists(email: str) -> bool:
 
-    statement = select(User).where(User.email == email)
-    existing_user = session.exec(statement).first()
+    with Session(engine) as session:
+        statement = select(User).where(User.email == email)
+        existing_user = session.exec(statement).first()
+    
     return existing_user == None
     
